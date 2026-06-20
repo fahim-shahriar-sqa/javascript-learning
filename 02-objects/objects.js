30245f234ca0feb5b88ee8a9fb0a3bffd5c4276d
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2026-06-20 19:59:19 +0600

    Lesson 02 - JavaScript objects

---
+++ 02-objects/objects.js
@@ -0,0 +1,23 @@
+const project = {
+    name: 'Restful Booker',
+    type: 'API Testing',
+    tool: 'Postman'
+};
+
+console.log(project.name);
+console.log(project.type);
+console.log(project.tool);
+
+const booking = {
+    firstname: 'John',
+    lastname: 'Smith',
+    totalprice: 1000
+};
+
+console.log(booking.firstname);
+console.log(booking.lastname);
+console.log(booking.totalprice);
+
+booking.totalprice = 1500;
+
+console.log(booking.totalprice);
