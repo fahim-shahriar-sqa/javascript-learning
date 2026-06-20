37b3ca3fb09c5deffbcaa146b9e4661668b23676
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2026-06-20 19:48:32 +0600

    Lesson 01 - JavaScript variables const and let

---
+++ 01-variables/exercise.js
@@ -0,0 +1,12 @@
+const baseUrl = 'https://restful-booker.herokuapp.com';
+const userName = 'admin';
+
+let bookingCount = 5;
+
+console.log(baseUrl);
+console.log(userName);
+console.log(bookingCount);
+
+bookingCount = 15;
+
+console.log(bookingCount);
---
+++ 01-variables/variables.js
@@ -0,0 +1,11 @@
+const company = 'OpenAI';
+const tool = 'k6';
+let testCount = 1;
+
+console.log(company);
+console.log(tool);
+console.log(testCount);
+
+testCount = 2;
+
+console.log(testCount);
