e466bdf93dfb8e6b0e62656eb026e8e28c9ac083
fahim-shahriar-sqa <work.fahimshahriar@gmail.com>
2026-06-21 09:03:12 +0600

    Lesson 07 - JavaScript template literals

---
+++ 07-template-literals.js
@@ -0,0 +1,7 @@
+const environment = "QA";
+const baseUrl = "https://api.company.com";
+const userId = 101;
+
+console.log(`Running API tests in ${environment} environment.`);
+
+console.log(`Fetching user from ${baseUrl}/users/${userId}.`);
